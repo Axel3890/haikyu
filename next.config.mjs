63b5defa51469638ef8f3521b0bcc4b1c6dab795
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-09-23 13:40:17 -0300

    probando

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['nextui.org'],
+    },
+  };
+  
+  export default nextConfig;
+  
